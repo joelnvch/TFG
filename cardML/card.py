448de814369f38be3fc804a8a4f09abc8e419b31
Joel <61578757+joelnvch@users.noreply.github.com>
2021-11-02 19:40:50 +0100

    base

---
+++ cardML/card.py
@@ -0,0 +1,45 @@
+import json
+import os
+from collections import Counter
+
+
+def load_cards(path):
+    keys = ["black", "blue", "yellow", "red", "green", "purple", "orange"]
+    cards = dict.fromkeys(keys)
+    cont = 0
+    for filename in os.listdir(path):
+        if filename.endswith(".json"):
+            json_name = os.path.join(path, filename)
+            with open(json_name, 'r', encoding="utf8") as card_file:
+                cards[keys[cont]] = []
+                card_data = json.loads(card_file.read())
+                for card in card_data:
+                    cards[keys[cont]].append(Card(**card))
+        cont += 1
+
+    return cards
+
+
+def get_card(name, list):
+    for card in list:
+        if card.name == name:
+            return card
+
+
+class Card:
+    def __init__(self, name, desc, costs, letters, value, card_color):
+        self.name = name
+        self.costs = costs
+        self.letters = letters
+        self.value = value
+        self.card_color = card_color
+
+    def __lt__(self, other):
+        return self.value >= other.value
+
+    def __str__(self):
+        return "\nName: " + self.name + \
+               "\nCosts: " + str(self.costs) + \
+               "\nLetters: " + str(self.letters) + \
+               "\nValue: " + str(self.value) + \
+               "\nCard Color: " + self.card_color
---
+++ cardML/main.py
@@ -0,0 +1,44 @@
+import card as card
+
+from board import *
+from card import load_cards
+
+# constants
+CARD_DATA_PATH = "cardData"
+
+
+def sort_by_highest(card_types):
+    for i in range(len(COLORS)):
+        card_list_by_color = card_types[COLORS[i]]
+        card_types[COLORS[i]] = sorted(card_list_by_color)
+
+        sum = 0
+        for card in card_list_by_color:
+            sum += card.value
+            spots = vars(card)
+
+
+if __name__ == '__main__':
+    all_cards = load_cards(CARD_DATA_PATH)
+    # sort_by_highest(all_cards)
+
+    board1 = Board()
+    board2 = Board()
+    board3 = Board()
+
+    card.get_card("Text Data", all_cards['black'])
+
+    board1.init_board(all_cards, [all_cards['black'][0], all_cards['blue'][0], all_cards['yellow'][0]])
+    print(board1)
+    board2.init_board(all_cards, [all_cards['black'][0], all_cards['blue'][0], all_cards['yellow'][0]])
+    board3.init_board(all_cards, [all_cards['black'][0], all_cards['blue'][0], all_cards['yellow'][0]])
+
+    # 0 : mayor beneficio
+    # 1 : menor coste
+    # 2 : mayor beneficio con el menor coste
+    calculated_cards1 = board1.build_board(0)
+    calculated_cards2 = board2.build_board(1)
+    calculated_cards3 = board3.build_board(2)
+
+    for card in calculated_cards1:
+        print(card)
