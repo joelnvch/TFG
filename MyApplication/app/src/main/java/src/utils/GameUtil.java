6d7b48358b1975b22a91975cc9684870f555e272
Joel <61578757+joelnvch@users.noreply.github.com>
2021-11-11 18:33:36 +0100

    Update GameUtil.java

--- MyApplication/app/src/main/java/src/utils/GameUtil.java
+++ MyApplication/app/src/main/java/src/utils/GameUtil.java
@@ -2,6 +2,35 @@ package src.utils;
 
 public class GameUtil {
     //pyBoard.callAttr("get_best_cards", "black")
+    /*
+    fun getBestCards(color: String): MutableList<CardView> {
+        var pythonFile = Python.getInstance().getModule("board")
+
+        val pyBestCards = pythonFile.callAttr("get_best_cards", color).asList()
+        return transformPyCardList(pyBestCards)
+    }
+
+    // better this than to call python and iterate list multiple times when it can be done just once when object initialization
+    fun getAllCards(color: String): MutableList<CardView> {
+        return board.allCards.getValue(color).values.toMutableList()
+    }
 
 
+    //METHOD CALLED IN THE SECOND SCREEN
+    fun setCard(color: String, cardId: Int) {
+        // update logic object
+        pyBoard.callAttr("set_card", color, cardId)
+        // update view object
+        board.spots[color] = board.allCards.getValue(color).getValue(cardId)
+    }
+
+    // AUX
+    fun transformPyCardList(pyCards: List<PyObject>): MutableList<CardView>{
+        val cards = mutableListOf<CardView>()
+
+        for (pyCard in pyCards)
+            cards.add(CardView(pyCard))
+
+        return cards
+    }    */
 }
